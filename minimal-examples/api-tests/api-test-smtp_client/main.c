017eb784ad095a162ca3013004c46ccc40a44a1b
Andy Green <andy@warmcat.com>
2019-06-26 14:32:46 +0100

    unit test sequencer

--- minimal-examples/api-tests/api-test-smtp_client/main.c
+++ minimal-examples/api-tests/api-test-smtp_client/main.c
@@ -1,5 +1,5 @@
 /*
- * lws-api-test-smtp_client
+ * lws-unit-tests-smtp-client
  *
  * Written in 2010-2019 by Andy Green <andy@warmcat.com>
  *
@@ -11,49 +11,152 @@
 
 #include <signal.h>
 
-static int interrupted, result = 1;
-static const char *recip;
+static int interrupted, result = 1, results[10], count_tests, count_passes;
+
+static int
+email_sent_or_failed(struct lws_smtp_email *email, void *buf, size_t len)
+{
+	free(email);
+
+	result = 0;
+
+	return 0;
+}
+
+/*
+ * The test helper calls this on the instance it created to prepare it for
+ * the test.
+ */
+
+static int
+smtp_test_instance_init(lws_abs_t *instance)
+{
+	lws_smtp_email_t *email = (lws_smtp_email_t *)
+					malloc(sizeof(*email) + 2048);
+
+	if (!email)
+		return 1;
+
+	/* attach an email to it */
+
+	memset(email, 0, sizeof(*email));
+	email->data = NULL /* email specific user data */;
+	email->email_from = "noreply@warmcat.com";
+	email->email_to = "andy@warmcat.com";
+	email->payload = (void *)&email[1];
+
+	lws_snprintf((char *)email->payload, 2048,
+			"From: noreply@example.com\n"
+			"To: %s\n"
+			"Subject: Test email for lws smtp-client\n"
+			"\n"
+			"Hello this was an api test for lws smtp-client\n"
+			"\r\n.\r\n", "andy@warmcat.com");
+	email->done = email_sent_or_failed;
+
+	if (lws_smtp_client_add_email(instance, email)) {
+		lwsl_err("%s: failed to add email\n", __func__);
+		return 1;
+	}
+
+	return 0;
+}
+
+/*
+ * from https://en.wikipedia.org/wiki/Simple_Mail_Transfer_Protocol
+ *
+ *		test vector sent to protocol
+ *				test vector received from protocol
+ */
+
+static lws_unit_test_packet_t test_send1[] = {
+	{
+		"220 smtp.example.com ESMTP Postfix",
+		smtp_test_instance_init, 34, LWS_AUT_EXPECT_RX
+	}, {
+				"HELO lws-test-client\x0a",
+		NULL, 21, LWS_AUT_EXPECT_TX
+	}, {
+		"250 smtp.example.com, I am glad to meet you",
+		NULL, 43, LWS_AUT_EXPECT_RX
+	}, {
+				"MAIL FROM: <noreply@warmcat.com>\x0a",
+		NULL, 33, LWS_AUT_EXPECT_TX
+	}, {
+		"250 Ok",
+		NULL, 6, LWS_AUT_EXPECT_RX
+	}, {
+				"RCPT TO: <andy@warmcat.com>\x0a",
+		NULL, 28, LWS_AUT_EXPECT_TX
+	}, {
+		"250 Ok",
+		NULL, 6, LWS_AUT_EXPECT_RX
+	}, {
+				"DATA\x0a",
+		NULL, 5, LWS_AUT_EXPECT_TX
+	}, {
+		"354 End data with <CR><LF>.<CR><LF>\x0a",
+		NULL, 35, LWS_AUT_EXPECT_RX
+	}, {
+				"From: noreply@example.com\n"
+				"To: andy@warmcat.com\n"
+				"Subject: Test email for lws smtp-client\n"
+				"\n"
+				"Hello this was an api test for lws smtp-client\n"
+				"\r\n.\r\n",
+		NULL, 27 + 21 + 39 + 1 + 47 + 5, LWS_AUT_EXPECT_TX
+	}, {
+		"250 Ok: queued as 12345\x0a",
+		NULL, 23, LWS_AUT_EXPECT_RX
+	}, {
+				"quit\x0a",
+		NULL, 5, LWS_AUT_EXPECT_TX
+	}, {
+		"221 Bye\x0a",
+		NULL, 7, LWS_AUT_EXPECT_RX |
+		   LWS_AUT_EXPECT_LOCAL_CLOSE |
+		   LWS_AUT_EXPECT_DO_REMOTE_CLOSE |
+		   LWS_AUT_EXPECT_TEST_END
+	}, {
+
+	}
+};
+
+
+static lws_unit_test_packet_t test_send2[] = {
+	{
+		"220 smtp.example.com ESMTP Postfix",
+		smtp_test_instance_init, 34, LWS_AUT_EXPECT_RX
+	}, {
+				"HELO lws-test-client\x0a",
+		NULL, 21, LWS_AUT_EXPECT_TX
+	}, {
+		"250 smtp.example.com, I am glad to meet you",
+		NULL, 43, LWS_AUT_EXPECT_RX
+	}, {
+				"MAIL FROM: <noreply@warmcat.com>\x0a",
+		NULL, 33, LWS_AUT_EXPECT_TX
+	}, {
+		"500 Service Unavailable",
+		NULL, 23, LWS_AUT_EXPECT_RX |
+		   LWS_AUT_EXPECT_DO_REMOTE_CLOSE |
+		   LWS_AUT_EXPECT_TEST_END
+	}, {
+
+	}
+};
+
+static lws_unit_test_t tests[] = {
+	{ "sending", test_send1, 3 },
+	{ "rejected", test_send2, 3 },
+	{ }
+};
 
 static void
 sigint_handler(int sig)
 {
 	interrupted = 1;
 }
-
-static int
-email_sent_or_failed(struct lws_smtp_email *email, void *buf, size_t len)
-{
-	/* you could examine email->data here */
-	if (buf)
-		lwsl_notice("%s: %.*s\n", __func__, (int)len, (const char *)buf);
-	else
-		lwsl_notice("%s:\n", __func__);
-
-	/* destroy any allocations in email */
-
-	free((char *)email->payload);
-
-	result = 0;
-	interrupted = 1;
-
-	return 0;
-}
-
-/*
- * We're going to bind to the raw-skt transport, so tell that what we want it
- * to connect to
- */
-
-static const lws_token_map_t smtp_raw_skt_transport_tokens[] = {
- {
-	.u = { .value = "127.0.0.1" },
-	.name_index = LTMI_PEER_V_DNS_ADDRESS,
- }, {
-	.u = { .lvalue = 25 },
-	.name_index = LTMI_PEER_LV_PORT,
- }, {
- }
-};
 
 static const lws_token_map_t smtp_protocol_tokens[] = {
  {
@@ -63,15 +166,29 @@ static const lws_token_map_t smtp_protocol_tokens[] = {
  }
 };
 
+const char *dnames[] = {
+	"INCOMPLETE",
+	"PASS",
+	"FAIL",
+	"FAIL(TIMEOUT)",
+	"FAIL(UNEXPECTED PASS)",
+	"SKIPPED"
+};
+
+void
+tests_completion_cb(const void *cb_user)
+{
+	interrupted = 1;
+}
 
 int main(int argc, const char **argv)
 {
 	int n = 1, logs = LLL_USER | LLL_ERR | LLL_WARN | LLL_NOTICE;
 	struct lws_context_creation_info info;
+	lws_test_sequencer_args_t args;
 	struct lws_context *context;
-	lws_abs_t abs, *instance;
-	lws_smtp_email_t email;
 	struct lws_vhost *vh;
+	lws_abs_t abs;
 	const char *p;
 
 	/* the normal lws init */
@@ -81,15 +198,8 @@ int main(int argc, const char **argv)
 	if ((p = lws_cmdline_option(argc, argv, "-d")))
 		logs = atoi(p);
 
-	p = lws_cmdline_option(argc, argv, "-r");
-	if (!p) {
-		lwsl_err("-r <recipient email> is required\n");
-		return 1;
-	}
-	recip = p;
-
 	lws_set_log_level(logs, NULL);
-	lwsl_user("LWS API selftest: SMTP client\n");
+	lwsl_user("LWS API selftest: SMTP client unit tests\n");
 
 	memset(&info, 0, sizeof info); /* otherwise uninitialized garbage */
 	info.port = CONTEXT_PORT_NO_LISTEN;
@@ -107,9 +217,7 @@ int main(int argc, const char **argv)
 		goto bail1;
 	}
 
-	/*
-	 * create an smtp client that's hooked up to real sockets
-	 */
+	/* create the smtp client */
 
 	memset(&abs, 0, sizeof(abs));
 	abs.vh = vh;
@@ -126,43 +234,25 @@ int main(int argc, const char **argv)
 
 	/* select the transport and bind its tokens */
 
-	abs.at = lws_abs_transport_get_by_name("raw_skt");
+	abs.at = lws_abs_transport_get_by_name("unit_test");
 	if (!abs.at) {
 		lwsl_err("%s: no raw_skt abstract transport\n", __func__);
 
 		goto bail1;
 	}
-	abs.at_tokens = smtp_raw_skt_transport_tokens;
 
-	instance = lws_abs_bind_and_create_instance(&abs);
-	if (!instance) {
-		lwsl_err("%s: failed to create SMTP client\n", __func__);
+	args.abs = &abs;
+	args.tests = tests;
+	args.results = results;
+	args.results_max = LWS_ARRAY_SIZE(results);
+	args.count_tests = &count_tests;
+	args.count_passes = &count_passes;
+	args.cb = tests_completion_cb;
+	args.cb_user = NULL;
+
+	if (lws_abs_unit_test_sequencer(&args)) {
+		lwsl_err("%s: failed to create test sequencer\n", __func__);
 		goto bail1;
-	}
-
-	/* attach an email to it */
-
-	memset(&email, 0, sizeof(email));
-	email.data = NULL /* email specific user data */;
-	email.email_from = "andy@warmcat.com";
-	email.email_to = recip;
-	email.payload = malloc(2048);
-	if (!email.payload) {
-		goto bail1;
-	}
-
-	lws_snprintf((char *)email.payload, 2048,
-			"From: noreply@example.com\n"
-			"To: %s\n"
-			"Subject: Test email for lws smtp-client\n"
-			"\n"
-			"Hello this was an api test for lws smtp-client\n"
-			"\r\n.\r\n", recip);
-	email.done = email_sent_or_failed;
-
-	if (lws_smtp_client_add_email(instance, &email)) {
-		lwsl_err("%s: failed to add email\n", __func__);
-		goto bail;
 	}
 
 	/* the usual lws event loop */
@@ -170,12 +260,16 @@ int main(int argc, const char **argv)
 	while (n >= 0 && !interrupted)
 		n = lws_service(context, 1000);
 
-bail:
+	lwsl_user("%s: %d tests %d fail\n", __func__, count_tests,
+			count_tests - count_passes);
+	for (n = 0; n < count_tests; n++)
+		lwsl_user("  test %d: %s\n", n, dnames[results[n]]);
 
 bail1:
-	lwsl_user("Completed: %s\n", result ? "FAIL" : "PASS");
+	lwsl_user("Completed: %s\n",
+		  !count_tests || count_passes != count_tests ? "FAIL" : "PASS");
 
 	lws_context_destroy(context);
 
-	return result;
+	return !count_tests || count_passes != count_tests;
 }
